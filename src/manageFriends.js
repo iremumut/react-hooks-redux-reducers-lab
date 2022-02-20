b9a37eb63898f298a4dc0ffa622cb40f1ce250ca
iremumut <irem2000.nur@gmail.com>
2022-02-21 02:05:41 +0300

    reducers

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,15 @@
-export function manageFriends(state, action) {
+const initialState = {friends: []};
+
+export function manageFriends(state = initialState, action) {
+  switch(action.type) {
+    case "friends/add":
+      return {friends: [...state.friends, {name: action.payload.name, hometown: action.payload.hometown, id: action.payload.id}]};
+    case "friends/remove":
+      return {friends: state.friends.filter((friend) => {
+        return friend.id !== action.payload;
+      })}
+    default: 
+      return state;
+  }
   // your code here
 }
